9f451157d7372fb5005d502f04977b0835d729b8
mauriciobrito7 <maubot01@gmail.com>
2021-09-02 08:57:31 -0400

    getting user info from redux

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,19 +9,20 @@ import {
 import { formatterCurrency } from "../../utils/index";
 import { currencies, locales } from "../../constants/index";
 import { ReactComponent as Coin } from "../../assets/coins.svg";
+import { connect } from "react-redux";
 
-function Header() {
+function Header({ userName, balance }) {
   return (
     <HeaderContainer>
       <Nav>
         <Balance>
           <Coin />
           <Amount>
-            {formatterCurrency(locales["US"], currencies["USD"], 2000)}
+            {formatterCurrency(locales["US"], currencies["USD"], balance)}
           </Amount>
         </Balance>
         <User>
-          Mauricio{" "}
+          {userName}
           <Avatar
             src="https://avatars.githubusercontent.com/u/12994188?v=4"
             alt="avatar"
@@ -32,4 +33,9 @@ function Header() {
   );
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  userName: state.user.userDetails.name,
+  balance: state.user.userDetails.balance,
+});
+
+export default connect(mapStateToProps, null)(Header);
