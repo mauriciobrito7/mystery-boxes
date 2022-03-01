0814cbdaa80dc05bc8cecc75b9c8e49ee7bf111d
Mauricio Brito <maubot01@gmail.com>
2022-03-01 19:18:19 -0300

    fix: card image responsive

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,23 +1,29 @@
-import { CardContainer, CardInfo, CardLabel, CardPrice } from "./Card.style";
+import {
+	CardContainer,
+	CardInfo,
+	CardLabel,
+	CardPrice,
+	CardImage,
+} from "./Card.style";
 import { formatterCurrency } from "../../utils/index";
 import { currencies, locales } from "../../constants/index";
 import { Link } from "react-router-dom";
 
 const Card = ({ id, boxImg, info, label, labelColor, price }) => {
-  return (
-    <CardContainer>
-      <Link to={`/box-detail/${id}`}>
-        <img src={boxImg} alt={label}/>
-        <CardInfo>{info}</CardInfo>
-        <CardLabel labelColor={labelColor}>
-          {label}
-          <CardPrice>
-            {formatterCurrency(locales["US"], currencies["USD"], price)}
-          </CardPrice>
-        </CardLabel>
-      </Link>
-    </CardContainer>
-  );
+	return (
+		<CardContainer>
+			<Link to={`/box-detail/${id}`}>
+				<CardImage src={boxImg} alt={label} />
+				<CardInfo>{info}</CardInfo>
+				<CardLabel labelColor={labelColor}>
+					{label}
+					<CardPrice>
+						{formatterCurrency(locales["US"], currencies["USD"], price)}
+					</CardPrice>
+				</CardLabel>
+			</Link>
+		</CardContainer>
+	);
 };
 
 export default Card;
