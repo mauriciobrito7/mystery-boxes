fc9697abac7b362b4f83804a253360e4f85872ea
Mauricio Brito <maubot01@gmail.com>
2022-03-04 11:29:18 -0300

    fix: remove and add functionality added

--- src/pages/Gifts/index.js
+++ src/pages/Gifts/index.js
@@ -5,81 +5,20 @@ import {
 	GiftsTitleContainer,
 	GiftsTitle,
 } from "./Gifts.styles";
-import booterBox from "../../assets/pokemon-gifts/booter-box.svg";
-import charizard from "../../assets/pokemon-gifts/charizard.svg";
-import pikachu from "../../assets/pokemon-gifts/pikachu.svg";
-import venusaur from "../../assets/pokemon-gifts/venusaur.svg";
-import pokemonPackageBlack from "../../assets/pokemon-gifts/pokemon-package-black.svg";
-import pokemonPackageRed from "../../assets/pokemon-gifts/pokemon-package-red.svg";
 import Gift from "../../components/Gift";
 import { Slug } from "../../constants";
+import { connect } from "react-redux";
 
-const myGifts = [
-	{
-		name: "Team Up Booster Box",
-		img: booterBox,
-		label: "POKEMON",
-		sellPrice: 1180,
-	},
-	{
-		name: "Charizard-Holo 1999",
-		img: charizard,
-		label: "POKEMON",
-		sellPrice: 80,
-	},
-	{
-		name: "Pikachu-Holo 1999",
-		img: pikachu,
-		label: "POKEMON",
-		sellPrice: 80,
-	},
-	{
-		name: "Venusaur-Holo 1999",
-		img: venusaur,
-		label: "POKEMON",
-		sellPrice: 80,
-	},
-	{
-		name: "2018 Pokemon Sun and Moon Black",
-		img: pokemonPackageBlack,
-		label: "POKEMON",
-		sellPrice: 1198.8,
-	},
-	{
-		name: "2019 Pokemon Sun and Moon Red",
-		img: pokemonPackageRed,
-		label: "POKEMON",
-		sellPrice: 1198.8,
-	},
-	{
-		name: "Venusaur-Holo 1999",
-		img: venusaur,
-		label: "POKEMON",
-		sellPrice: 80,
-	},
-	{
-		name: "2018 Pokemon Sun and Moon Black",
-		img: pokemonPackageBlack,
-		label: "POKEMON",
-		sellPrice: 1198.8,
-	},
-	{
-		name: "2019 Pokemon Sun and Moon Red",
-		img: pokemonPackageRed,
-		label: "POKEMON",
-		sellPrice: 1198.8,
-	},
-];
-
-function Gifts() {
+function Gifts({ user }) {
+	const { gifts } = user.userDetails;
 	return (
 		<GiftsContainer>
 			<GiftsTitleContainer>
 				<GoBack url={Slug.Home} />
-				<GiftsTitle>All your Gifts</GiftsTitle>
+				<GiftsTitle>All your Gifts ({gifts.length})</GiftsTitle>
 			</GiftsTitleContainer>
 			<GiftsCollection>
-				{myGifts.map((gift) => (
+				{gifts.map((gift) => (
 					<Gift key={gift.name} {...gift} />
 				))}
 			</GiftsCollection>
@@ -87,4 +26,8 @@ function Gifts() {
 	);
 }
 
-export default Gifts;
+const mapStateToProps = (state) => ({
+	user: state.user,
+});
+
+export default connect(mapStateToProps, null)(Gifts);
