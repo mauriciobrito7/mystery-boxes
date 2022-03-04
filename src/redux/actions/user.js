3ea1ee5fa21c018328cadfd9ee2d77610641c76e
Mauricio Brito <maubot01@gmail.com>
2022-03-04 11:27:11 -0300

    fix: remove and add gifts

--- src/redux/actions/user.js
+++ src/redux/actions/user.js
@@ -1,11 +1,21 @@
-import { ADD_BALANCE, DISCOUNT_BALANCE } from "../types";
+import { ADD_BALANCE, DISCOUNT_BALANCE, ADD_GIFT, REMOVE_GIFT } from "../types";
 
 export const addBalance = (amount) => ({
-  type: ADD_BALANCE,
-  payload: amount,
+	type: ADD_BALANCE,
+	payload: amount,
 });
 
 export const discountBalance = (amount) => ({
-  type: DISCOUNT_BALANCE,
-  payload: amount,
+	type: DISCOUNT_BALANCE,
+	payload: amount,
+});
+
+export const addGift = (gift) => ({
+	type: ADD_GIFT,
+	payload: gift,
+});
+
+export const removeGift = (gift) => ({
+	type: REMOVE_GIFT,
+	payload: gift,
 });
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,36 +1,54 @@
 import * as types from "../types";
 
 const INITIAL_STATE = {
-  userDetails: {
-    name: "Mauricio",
-    avatar: "https://avatars.githubusercontent.com/u/12994188?v=4",
-    balance: 2000,
-  },
+	userDetails: {
+		name: "Mauricio",
+		avatar: "https://avatars.githubusercontent.com/u/12994188?v=4",
+		balance: 2000,
+		gifts: [],
+	},
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case types.ADD_BALANCE:
-      return {
-        ...state,
-        userDetails: {
-          ...state.userDetails,
-          balance: state.userDetails.balance + action.payload,
-        },
-      };
+	switch (action.type) {
+		case types.ADD_BALANCE:
+			return {
+				...state,
+				userDetails: {
+					...state.userDetails,
+					balance: state.userDetails.balance + action.payload,
+				},
+			};
 
-    case types.DISCOUNT_BALANCE:
-      return {
-        ...state,
-        userDetails: {
-          ...state.userDetails,
-          balance: state.userDetails.balance - action.payload,
-        },
-      };
-
-    default:
-      return state;
-  }
+		case types.DISCOUNT_BALANCE:
+			return {
+				...state,
+				userDetails: {
+					...state.userDetails,
+					balance: state.userDetails.balance - action.payload,
+				},
+			};
+		case types.ADD_GIFT:
+			return {
+				...state,
+				userDetails: {
+					...state.userDetails,
+					gifts: [...state.userDetails.gifts, action.payload],
+				},
+			};
+		case types.REMOVE_GIFT:
+			return {
+				...state,
+				userDetails: {
+					...state.userDetails,
+					gifts: state.userDetails.gifts.filter(
+						(gift) => gift.name !== action.payload.name
+					),
+				},
+			};
+		default:
+			return state;
+	}
 };
 
 export default userReducer;
