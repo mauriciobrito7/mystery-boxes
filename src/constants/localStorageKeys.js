7ef1dbc363952da78015896db38ff9b947c17655
Mauricio Brito <maubot01@gmail.com>
2022-03-05 23:19:36 -0300

    feat: local storage functionality added

---
+++ src/constants/localStorageKeys.js
@@ -0,0 +1,2 @@
+export const MY_GIFTS = "my_gifts";
+export const MY_BALANCE = "my_balance";
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,17 +1,20 @@
 import * as types from "../types";
+import { MY_GIFTS, MY_BALANCE } from "../../constants";
+import { getFromLocalStorage, setLocalStorage } from "../../utils";
 
 const INITIAL_STATE = {
 	userDetails: {
 		name: "Mauricio",
 		avatar: "https://avatars.githubusercontent.com/u/12994188?v=4",
-		balance: 2000,
-		gifts: [],
+		balance: getFromLocalStorage(MY_BALANCE, 2000),
+		gifts: getFromLocalStorage(MY_GIFTS, []),
 	},
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
 	switch (action.type) {
 		case types.ADD_BALANCE:
+			setLocalStorage(MY_BALANCE, state.userDetails.balance + action.payload);
 			return {
 				...state,
 				userDetails: {
@@ -21,6 +24,7 @@ const userReducer = (state = INITIAL_STATE, action) => {
 			};
 
 		case types.DISCOUNT_BALANCE:
+			setLocalStorage(MY_BALANCE, state.userDetails.balance - action.payload);
 			return {
 				...state,
 				userDetails: {
@@ -29,6 +33,7 @@ const userReducer = (state = INITIAL_STATE, action) => {
 				},
 			};
 		case types.ADD_GIFT:
+			setLocalStorage(MY_GIFTS, [...state.userDetails.gifts, action.payload]);
 			return {
 				...state,
 				userDetails: {
@@ -37,13 +42,15 @@ const userReducer = (state = INITIAL_STATE, action) => {
 				},
 			};
 		case types.REMOVE_GIFT:
+			const filteredGifts = state.userDetails.gifts.filter(
+				(gift) => gift.name !== action.payload.name
+			);
+			setLocalStorage(MY_GIFTS, filteredGifts);
 			return {
 				...state,
 				userDetails: {
 					...state.userDetails,
-					gifts: state.userDetails.gifts.filter(
-						(gift) => gift.name !== action.payload.name
-					),
+					gifts: filteredGifts,
 				},
 			};
 		default:
